c5c5da915d968749451cfffa7d93d3fd30b4ee46
Jadson Rocha <jadsonrocha599@gmail.com>
2025-09-29 12:49:06 -0300

    feat(nucleo): cria a estrutura do banco de dados em memoria

--- Application.java
+++ Application.java
@@ -1,2 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    private List<Eventos> eventos;
+    private List<Participantes> participantes;
+
+    public Application() {
+        this.eventos = new ArrayList<>();
+        this.participantes = new ArrayList<>();
+    }
+
+    public void run() {
+        System.out.println("Sistema de Gestão de Eventos iniciado!");
+    }
 }
