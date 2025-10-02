56ec7531fa213aeac91457ba0b30b2acb99afed7
Gabriela Carvalho <gabriela.fdecarvalho@gmail.com>
2025-10-01 21:38:00 -0300

    Alteração do construtor, remoção métodos desnecessários e adição do toString para imprimir Ingresso.

--- src/Ingressos.java
+++ src/Ingressos.java
@@ -1,45 +1,55 @@
+
 public class Ingressos {
 
-    private boolean especial;
+    private boolean  especial;
     private String codigo;
-    private Participantes Participante;
+    private Participantes participante;
     private boolean foiUtilizado;
 
-    public Ingressos(Participantes comprador, String codigo, Boolean especial){
-        especial = especial;
-        codigo = codigo;
-        Participante = comprador;
+    public Ingressos(Participantes comprador, String codigo, boolean especial){
+        this.especial = especial;
+        this.codigo = codigo;
+        this.participante = comprador;
+        this.foiUtilizado = false;
     }
 
     public boolean isEspecial () {
         return especial;
     }
 
-    public void setEspecial (boolean especial) {
-        this.especial = especial;
-    }
-
     public String getCodigo () {
         return codigo;
     }
 
-    public void setCodigo (String codigo) {
-        this.codigo = codigo;
-    }
-
     public Participantes getParticipante () {
-        return Participante;
+        return participante;
     }
 
-    public void setParticipante (Participantes participante) {
-        Participante = participante;
-    }
-
-    public boolean getfoiUtilizado () {
+    public boolean isFoiUtilizado () {
         return foiUtilizado;
     }
 
-    public void setfoiUtilizado (boolean foiUtilizado) {
-        foiUtilizado = foiUtilizado;
+    public void setFoiUtilizado(boolean usado) {
+        this.foiUtilizado = usado;
+    }
+
+   @Override
+    public String toString() {
+        if (participante != null) {
+            if (especial == true) {
+                return codigo + " - Especial - " + participante.getNome();
+            } else {
+                return codigo + " - Normal - " + participante.getNome();
+            }
+        } else {
+            if (especial) {
+                return codigo + " - Especial (ingresso disponível)";
+            } else {
+                return codigo + " - Normal (ingresso disponível)";
+            }
+        }
     }
 }
+
+ 
+
